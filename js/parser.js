4e4452c6b8b4180e7761644e6e5a44e3e91c23dc
milu <milu@qburst.com>
2015-06-17 09:52:53 +0530

    jquery file modified

--- js/parser.js
+++ js/parser.js
@@ -1,34 +1,35 @@
 $(document).ready(function() {
-var progressElem = $('#progressCounter');
-var URL = "https://api.github.com/users/mralexgray/repos";
-$("#loading").hide();
-var obj=$.ajax({
-    type: 'GET',
-    dataType: 'json',
-    url: URL,
-    cache: false,
-    beforeSend: function () {
-        $("#table").hide();
-        $('#loading').show();
-        $("#followerstable").hide();
-        $("#loadingfollowers").hide();
-        $("#hide").hide();
+    var progressElem = $('#progressCounter');
+    var URL = "https://api.github.com/users/mralexgray/repos";
+    $("#loading").hide();
+    var obj=$.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: URL,
+        cache: false,
+        beforeSend: function () {
+            $("#table").hide();
+            $('#loading').show();
+            $("#followerstable").hide();
+            $("#loadingfollowers").hide();
+            $("#hide").hide();
 
-    },
-    complete: function () {
-        $("#loading").hide();
-        $("#table").show();
-    },
-    success: function (json) {
+        },
+    })
+    .done(function (json) {
 
         for (var i = json.length - 1; i >= 0; i--) {
-        
-        $("#table").append("<tr><th>" + json[i].name + "</th><th>" + json[i].full_name + "</th><th>" + json[i].size +
-         "</th><th>" + json[i].created_at + "</th><th>" + json[i].language + "</th><th>" + json[i].has_issues + "</th><th>" 
-         + json[i].has_downloads + "</th><th>" + json[i].watchers + "</th></tr>");
+
+            $("#table").append("<tr><th>" + json[i].name + "</th><th>" + json[i].full_name + "</th><th>" + json[i].size +
+               "</th><th>" + json[i].created_at + "</th><th>" + json[i].language + "</th><th>" + json[i].has_issues + "</th><th>" 
+               + json[i].has_downloads + "</th><th>" + json[i].watchers + "</th></tr>");
         }
 
         $('.followersButton').click(function() {
+            ListFollowers();
+        });
+        function ListFollowers()
+        {
             var URL = "https://api.github.com/users/mralexgray/followers";
             $("#loading").hide();
             var obj=$.ajax({
@@ -40,30 +41,36 @@ var obj=$.ajax({
                     $("#followerstable").hide();
                     $('#loadingfollowers').show();
                     $("#hide").hide();
-                },
-                complete: function () {
-                    $("#loadingfollowers").hide();
-                    $("#followerstable").show();
-                    $("#hide").show();
-                },
-                success: function (json) {
-                    $("#followerstable").append("<tr><th>Name</th><th>ID</th></tr>");
-                    for (var i = json.length - 1; i >= 0; i--) {
-                       $("#followerstable").append("<tr><th>" + json[i].login + "</th><th>" + json[i].id + "</th></tr>");
-                    }; 
-                }
+                },})
+
+            .done(function (json) {
+                $("#followerstable").append("<tr><th>Name</th><th>ID</th></tr>");
+                for (var i = json.length - 1; i >= 0; i--) {
+                 $("#followerstable").append("<tr><th>" + json[i].login + "</th><th>" + json[i].id + "</th></tr>");
+             }; 
+         })
+            .complete(function () {
+                $("#loadingfollowers").hide();
+                $("#followerstable").show();
+                $("#hide").show();
             });
-        });
+        }
         $("#hide").click(function()
-        {   alert("hide");
-            $("#followerstable").empty();
+            {   $("#followerstable").empty();
             $("#hide").hide();
-        });   
-    }
-});
+        }); 
+    })  
+
+
+    .complete(function () {
+       $("#loading").hide();
+       $("#table").show();
+    });
 
 });
 
- 
+
+
+
 
 
